6bb6b65b4b85dd83375bda6b97addae133473300
shamar-morrison <shamar.webdev@gmail.com>
2022-03-18 13:58:02 -0500

    Added Banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,26 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+
+interface BannerProps {
+  purpose: string;
+}
+
+const Banner = ({ purpose }: BannerProps) => {
+  return (
+    <Flex flexWrap={'wrap'} justifyContent="center" alignItems={'center'} m="10">
+      {purpose}
+    </Flex>
+  );
+};
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner purpose="For Sale" />
+      <Banner purpose="For Rent" />
+    </div>
+  );
 };
 
 export default Home;
